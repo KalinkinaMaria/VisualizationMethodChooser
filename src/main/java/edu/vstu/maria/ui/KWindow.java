6fd2c5b92a9b0a653278c74b634320a0c364d49b
Nikita Nikitsky <nikitskynikita@gmail.com>
2017-06-12 18:25:28 +0300

    Remove method show from abstract class for windows

--- src/main/java/edu/vstu/maria/ui/KWindow.java
+++ src/main/java/edu/vstu/maria/ui/KWindow.java
@@ -14,16 +14,16 @@ public abstract class KWindow {
         this.currentWindow = new Stage();
     }
 
+    public KWindow(@NotNull final Stage stage) {
+        this.currentWindow = stage;
+    }
+
     public final String getTitle() {
         return this.currentWindow.getTitle();
     }
 
     public void setTitle(@NotNull final String value) {
         this.setTitle(value);
-    }
-
-    public void show(@NotNull final Stage ownerWindow) {
-        this.currentWindow.initOwner(ownerWindow);
     }
 
 }
